03087cd63a663657cf6ad1c11baea36006116030
RazelRaz <razel16dcm@gmail.com>
2024-08-21 19:48:14 +0600

    listed employee in list employee

--- pages/list-employee.php
+++ pages/list-employee.php
@@ -1,4 +1,10 @@
-
+<?php 
+  global $wpdb;
+  $employees = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}ems_system", ARRAY_A );
+  echo '<pre>';
+  print_r($employees);
+  echo '</pre>';
+?>
 
 <div class="container">
   <h2>List Employee</h2>
@@ -20,20 +26,32 @@
         </thead>
         <tbody>
 
-          <tr>
-            <td>1</td>
-            <td>John Doe</td>
-            <td>john@example.com</td>
-            <td>4569867543</td>
-            <td>Male</td>
-            <td>Web Developer</td>
-            <td>
-              <a href="" class="btn btn-warning">Edit</a>
-              <a href="" class="btn btn-danger">Delete</a>
-              <a href="" class="btn btn-info">View</a>
-            </td>
-          </tr>
-          
+          <?php 
+
+            if( count($employees) > 0 ) {
+              foreach ( $employees as $employee ) {
+                ?>
+                  <tr>
+                    <td><?php echo $employee['id'] ?></td>
+                    <td><?php echo $employee['name'] ?></td>
+                    <td><?php echo $employee['email'] ?></td>
+                    <td><?php echo $employee['email'] ?></td>
+                    <td><?php echo ucfirst($employee['gender']) ?></td>
+                    <td><?php echo $employee['designation'] ?></td>
+                    <td>
+                      <a href="admin.php?page=employee-system&action=edit&empId=<?php echo $employee['id'] ?>" class="btn btn-warning">Edit</a>
+                      <a href="admin.php?page=list-employee&action=delete&empId=<?php echo $employee['id'] ?>" class="btn btn-danger">Delete</a>
+                      <a href="admin.php?page=employee-system&action=view&empId=<?php echo $employee['id'] ?>" class="btn btn-info">View</a>
+                    </td>
+                  </tr>
+                <?php
+              }
+            }else {
+              echo 'No Employees Found';
+            }
+
+          ?>
+
         </tbody>
       </table>
     </div>
