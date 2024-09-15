dff538f6c8918a1c165bf4c0719e8ee9230c4dbe
RazelRaz <razel16dcm@gmail.com>
2024-09-15 20:13:05 +0600

    delete method

--- pages/list-employee.php
+++ pages/list-employee.php
@@ -1,9 +1,25 @@
 <?php 
   global $wpdb;
-  $employees = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}ems_system", ARRAY_A );
+  $message = '';
+
+  
+  
   // echo '<pre>';
   // print_r($employees);
   // echo '</pre>';
+
+  // Delete block
+  if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
+
+    if ( isset( $_POST['emp_del_id'] ) && !empty( $_POST['emp_del_id'] ) ) {
+      $wpdb->delete("{$wpdb->prefix}ems_system", array(
+        'id' => intval($_POST['emp_del_id']),
+      ) );
+      $message = 'Employee Deleted Successfully...';
+    }
+  }
+
+  $employees = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}ems_system", ARRAY_A );
 ?>
 
 <div class="container">
@@ -12,6 +28,17 @@
   <div class="panel panel-primary">
       <div class="panel-heading">Employee List</div>
       <div class="panel-body">
+
+      <?php 
+        if ( !empty( $message ) ) {
+          ?>
+            <div class="alert alert-success">
+              <?php echo $message; ?>
+            </div>
+          <?php
+        } 
+      ?>
+
       <table class="table" id="tbl-employee">
         <thead>
           <tr>
@@ -40,7 +67,19 @@
                     <td><?php echo $employee['designation'] ?></td>
                     <td>
                       <a href="admin.php?page=employee-system&action=edit&empId=<?php echo $employee['id'] ?>" class="btn btn-warning">Edit</a>
-                      <a href="admin.php?page=list-employee&action=delete&empId=<?php echo $employee['id'] ?>" class="btn btn-danger">Delete</a>
+
+
+                      <form id="frm-delete-employee-<?php echo $employee['id'] ?>" method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>?page=list-employee">
+                        <input type="hidden" name="emp_del_id" value="<?php echo $employee['id'] ?>" >
+                      </form>
+                      <a href="javascript:void(0)" 
+                      onclick="
+                        if( confirm('Are You Sure??')){
+                            jQuery('#frm-delete-employee-<?php echo $employee['id'] ?>').submit();
+                        }
+                      " 
+                      class="btn btn-danger">Delete</a>
+
                       <a href="admin.php?page=employee-system&action=view&empId=<?php echo $employee['id'] ?>" class="btn btn-info">View</a>
                     </td>
                   </tr>
